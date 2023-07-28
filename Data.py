469505f2bb551ac07776487ef9c7319e182d8140
tomyim-123 <116877697+tomyim-123@users.noreply.github.com>
2023-07-28 14:07:42 +0800

    Create Data.py

---
+++ Data.py
@@ -0,0 +1,12 @@
+def GetData():
+  print('Connect to database')
+def Process_data(data):
+    def clean_data(data):
+        return [x for x in data if x is not None]
+
+    def sort_data(data):
+        return sorted(data)
+
+    data = clean_data(data)
+    data = sort_data(data)
+    return data
